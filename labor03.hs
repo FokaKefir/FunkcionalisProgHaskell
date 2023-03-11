0d04280b9f7716dbda978dfee0c4040c63a648f5
david <babosdavid8@gmail.com>
2023-03-11 18:19:39 +0200

    Lab 3

--- labor03.hs
+++ labor03.hs
@@ -26,3 +26,54 @@ myMaximum (k : vs)
     where 
         maxi = myMaximum vs 
 
+myListPos :: [a] -> Int -> a 
+myListPos ls k
+    | (length ls) <= k = error "index error"
+    | k == 0 = head ls
+    | otherwise = myListPos (tail ls) (k - 1)
+
+myAppendLists :: [a] -> [a] -> [a]
+myAppendLists [] ls = ls
+myAppendLists ls1 ls2
+    | null ls1 = ls2
+    | otherwise = myAppendLists ls (k : ls2)
+    where
+        k = last ls1
+        ls = init ls1
+
+myPalindrome :: Eq a => [a] -> Bool
+myPalindrome [] = True
+myPalindrome [k] = True
+myPalindrome (f : ve)
+    | f /= l = False
+    |otherwise = myPalindrome (init ve)
+    where 
+        l = last ve
+        
+numberOfDigits :: (Integral t) => t -> t
+numberOfDigits 0 = 0
+numberOfDigits k = 1 + numberOfDigits (k `div` 10)
+    
+putBack :: [a] -> [a]
+putBack [] = []
+putBack (k : ve) = ve ++ [k]
+
+
+average :: Fractional a => [a] -> a
+average ls = auxavg ls 0 0
+    where 
+        auxavg :: (Fractional a) => [a] -> a -> a -> a
+        auxavg [] s n = s / n
+        auxavg (k : ve) s n = auxavg ve (s + k) (n + 1)
+
+
+decToBase :: Integral t => t -> t -> [t]
+decToBase 0 b = []
+decToBase n b = (decToBase (n `div` b) b) ++ [n `mod` b]
+
+baseToDec :: Integral t => [t] -> t -> t
+baseToDec ls b = auxBTD ls b (b ^ (length ls - 1))
+    where
+        auxBTD :: Integral t => [t] -> t -> t -> t
+        auxBTD [] b bpow = 0
+        auxBTD (k : ls) b bpow = (k * bpow) + auxBTD ls b (bpow `div` b)
