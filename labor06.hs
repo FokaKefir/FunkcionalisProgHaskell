7ce0f3f7de7444797291ff16fcc6c6131c0c882e
FokaKefir <babosdavid8@gmail.com>
2023-05-08 16:15:14 +0300

    Lab 6

--- labor06.hs
+++ labor06.hs
@@ -1,3 +1,8 @@
+import Control.Exception
+import System.IO
+import System.IO (hPutStr)
+
+
 numsBetweenTwoNumber :: IO()
 numsBetweenTwoNumber = do
     line <- getLine
@@ -38,3 +43,62 @@ printFirstNFibNumber = do
     let n = read line :: Int
     let fibLst = fibFirstN n
     mapM_ (\x -> putStr (show x ++ " ")) fibLst
+
+
+fel1 = do
+    putStr "n = "
+    temp <- getLine
+    let n = read temp :: Int
+    print n
+
+fel2 = do
+    putStr "n1, n2 = "
+    temp <- getLine
+    let [tn1, tn2] = words temp
+    let n1 = read tn1 :: Int
+    let n2 = read tn2 :: Int
+    print(n1, n2)
+
+fel3 = do
+    putStr "n1, n2 = "
+    temp <- getLine
+    let tls = words temp
+    let [n1, n2] = map read tls :: [Int]
+    print(n1, n2)
+
+fel4 = do
+    catch (do
+        putStr "n1, n2 = "
+        temp <- getLine
+        let tls = words temp
+        let [n1, n2] = map read tls :: [Int]
+        print(n1, n2)
+        ) fgcatch
+            where
+                fgcatch :: SomeException -> IO ()
+                fgcatch e = do putStrLn $ "Error: " ++ show e 
+
+primT :: (Integral a) => a -> a -> Bool
+primT k nr
+    | nr <= 1 = error "hibas bemenet"
+    | nr == 2 = True
+    | even nr = False
+    | nr < k * k = True
+    | mod nr k == 0 = False
+    | otherwise = primT (k + 2) nr
+
+primesBetweenInterval = catch (do
+        putStr "n, m = "
+        line <- getLine
+        let [a, b] = map read (words line) :: [Int]
+        let ls = [x | x <- [a..b], primT 3 x]
+        mapM_ print ls
+    ) fgcatch
+        where
+            fgcatch :: SomeException -> IO ()
+            fgcatch e = do putStrLn $ "Error: " ++ show e 
+
+
+fibWriteFile = do
+    fout <- openFile "fibs.txt" WriteMode
+    mapM_ hPutStr $ fibFirstN 10
